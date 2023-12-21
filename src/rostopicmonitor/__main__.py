4a93533652cfa16fad8c3b936bc454199b946e3d
anetczuk <anetczuk@o2.pl>
2023-12-21 22:02:38 +0100

    Move application entry point

--- src/rostopicmonitor/__main__.py
+++ src/rostopicmonitor/__main__.py
@@ -1,10 +1,15 @@
-#!/usr/bin/env python3
 #
 # Copyright (c) 2023, Arkadiusz Netczuk <dev.arnet@gmail.com>
 # All rights reserved.
 #
 # This source code is licensed under the BSD 3-Clause license found in the
 # LICENSE file in the root directory of this source tree.
+#
+
+#
+# Entry file when package is used as the main program.
+# can be executed in following way:
+#        python -m <package>
 #
 
 import sys
